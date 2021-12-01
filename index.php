40befbbad1400d70b961ff86c4ad28b4acb08ebe
aat007 <aat_007@hotmail.com>
2021-12-01 22:12:39 +0300

    Update index.php

--- index.php
+++ index.php
@@ -1,18 +1,27 @@
 <?php
 
-$name=_POST['name'];
-$pass=_POST['pass'];
-$con= new mysqli("localhost","root","root","books");
-if($con->connect_error){
-    die('connection failed : ' .$con->connect_error);
-}else{ 
-   $stmt =$con->prepare("insert into user (username , password) values(?,?)"); 
-   $stmt ->bind_param("ss",$name,$pass);
-   $stmt ->execute();
-   echo "login suuccessfuley";
-   $stmt ->close();
-   $con ->close();
+// $name=_POST['name'];
+// $pass=_POST['pass'];
+// $con= new mysqli("localhost","root","root","books");
+// if($con->connect_error){
+//     die('connection failed : ' .$con->connect_error);
+// }else{ 
+//    $stmt =$con->prepare("insert into user (username , password) values(?,?)"); 
+//    $stmt ->bind_param("ss",$name,$pass);
+//    $stmt ->execute();
+//    echo "login suuccessfuley";
+//    $stmt ->close();
+//    $con ->close();
+// }
+
+$server = "localhost";
+$database = "books";
+$username = "root";
+$password = "";
+$mysqli = mysqli_connect($server, $username, $password, $database);
+if (mysqli_connect_errno()) {
+    printf("Connect failed: %s\n", mysqli_connect_error());
+    exit();
 }
 
 
-?>
